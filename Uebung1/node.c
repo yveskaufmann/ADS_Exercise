e9bf3b05c0991ba876ad74e666f055c65c4c64e8
Yves Kaufmann <fxdapokalypse@googlemail.com>
2014-06-22 13:43:39 +0200

    Provides the doygen comments for the node.h and node.c.

--- Uebung1/node.c
+++ Uebung1/node.c
@@ -1,33 +1,87 @@
 /**
-	
-	Project: List
-	File: node.c
-	Purpose: 
-
-	@author fxdapokalypse
-	@version 1.0 06.06.2014
-*/
+ * The default implementation of single and double linked node.
+ * This node implemenations is generic which means the
+ * implementation for single and douple node list is the same.
+ * Hence the ::Node_create function creates the corresponding struct
+ * which fit the needs for a single or double linked node.
+ *
+ * NOTE: the functions ::Node_setPrev and ::Node_getPrev are only
+ * callable if the specified node is a double linked list.
+ *
+ * @file node.c
+ * @author Yves Kaufmann
+ * @date 12 Jun 2014
+ */
 
 #include "node.h"
-
+/**
+ * This global value is provided by <errno.h> and contains
+ * the error number of the last occurred error. This
+ * value is used for determining the error message of a occured error.
+ */
 extern errno;
 
+/**
+ * Struct for single linked list.
+ */
 struct NodeSingleLinked {
+	/**
+	 * A boolean value which lets you know that this node is a single linked node
+	 * if the value is false.
+	 *
+	 * This have to be the default value of a single linked
+	 * node in order to determine the type of a specified node.
+	 */
 	bool isDoupleLinkedNode;
+	/**
+	 * The data pointer which points to the the data of this node.
+	 */
 	void *data;
+	/**
+	 * The node handle of the next node of this node.
+	 *
+	 * A next with the value NULL lets you know
+	 * that this node doesn't have a successor.
+	 */
 	Node next;
 }; 
 
+/**
+ * Struct for double linked list.
+ */
 struct NodeDoupleLinked {
+	/**
+	 * A boolean value which lets you know that this node is a double linked node
+	 * if the value is true.
+	 *
+	 * This have to be the default value of a double linked
+	 * node in order to determine the type of a specified node.
+	 */
 	bool isDoupleLinkedNode;
+	/**
+	 * The data pointer which points to the the data of this node.
+	 */
 	void *data;
+	/**
+	 * The node handle of the next node of this node.
+	 *
+	 * A next with the value NULL lets you know
+	 * that this node doesn't have a successor.
+	 */
 	Node next;
+	/**
+	 * The node handle of the previouse node of this node.
+	 *
+	 * A prev with the value NULL lets you know
+	 * that this node doesn't have a predecessor.
+	 */
 	Node prev;
 };
 
 Node Node_create(void *data, bool isDoupleLinkedNode) {
 	Node node = NULL;
 	if(isDoupleLinkedNode) {
+		// let's allocate the singly linked node
 		node = malloc(sizeof(struct NodeDoupleLinked));
 		if(node == NULL) {
 			fprintf(stderr, "DoupleLinkedNode creation failed: \"%s\"", strerror(errno));
@@ -36,6 +90,7 @@ Node Node_create(void *data, bool isDoupleLinkedNode) {
 		node->isDoupleLinkedNode = true;
 		Node_setPrev(node, NULL);
 	} else {
+		// let's allocate the singly linked node
 		node = malloc(sizeof(struct NodeSingleLinked));
 		if(node == NULL) {
 			fprintf(stderr, "SingleLinkedNode creation failed: \"%s\"", strerror(errno));
